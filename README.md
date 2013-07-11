9e09ab8a94c250aa3de9eb150a9149228eac8fca
Aaron <aaronsgh@gmail.com>
2013-07-12 01:50:53 +0530

    Everything works, and I am mostly sure it is all correct...

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Copyright 2013 Aaron Graham-Horowitz
 JavaScript app calculates Probability Mass Functions and Cumulative
 Distribution Functions for the results of a die roll - any number of
 dice, any number of sides on each die.  Also considers other aggregate
-(Min, Max) to be applied to the rolled numbers in place of summing them.
+functions (Min, Max) to be applied to the rolled numbers in place of
+summing them.
 
 Uses Highcharts library for display.
--- dice.js
+++ dice.js
@@ -7,8 +7,8 @@
 
 /* Usage:
  Output goes to <div id="diceplot"></div>.
- Draw PMF by calling makePlot(funcname, dicestring)
- where funcname is 'Sum', 'Min', or 'Max'
+ Draw PMF by calling makePlot(dist, funcname, dicestring)
+ where dist is 'PDF', 'CDF', or 'SIG', funcname is 'Sum', 'Min', or 'Max',
  and dicestring denotes the dice to roll e.g. '3d6, 1d20'.
 */
 $(function() {
@@ -19,7 +19,6 @@ $(function() {
     var pattern=/\d*d\d+/ig;
     var dice = [];
     dice.minroll = 0;
-    dice.maxroll = 0;
     dice.stringform = "";
     var die, roll;
     while ((roll=pattern.exec(s)) !== null)
@@ -31,7 +30,6 @@ $(function() {
         die[0] = parseInt(die[0]);
       die[1] = parseInt(die[1]);
       dice.minroll += die[0];
-      dice.maxroll += die[0] * die[1];
       dice.stringform += die[0] + "d" + die[1] + ", ";
       dice.push(die);
     }
@@ -50,8 +48,10 @@ $(function() {
   // Return convolution of oldpmf with a new die roll
   // NOTE: This is a highly specialized convolution function
   // not useful outside this context
-  function convolute(oldpmf, sides, support) {
+  // TODO: Abstract out commonalities with convomax, convomin
+  function convolute(oldpmf, sides) {
     var pmf = [];
+    var support = oldpmf.length + sides - 1;
     var cumsum = 0;
     for (var z=0; z < support; z++)
     {
@@ -64,31 +64,31 @@ $(function() {
     return pmf;
   }
 
-  var funcmap = {
-    sum : convolute
+  function convomax(oldpmf, sides) {
+    var oldcdf = getCdf(oldpmf);
+    var pmf = [];
+    var support = Math.max(oldpmf.length, sides);
+    var p = 0;
+    for (var z=0; z < support; z++)
+    {
+      if (z < oldpmf.length && z < sides)
+        p = (oldpmf[z] * z + oldcdf[z]) / sides;
+      else if (z < oldpmf.length)
+        p = oldpmf[z];
+      else if (z < sides)
+        p = 1 / sides;
+      pmf.push(p);
+    }
+    return pmf;
   }
 
-  // Return Probability Mass Function over function of dice roll
-  function getPmf(funcname, dice) {
-    var sides;
+  function convomin(oldpmf, sides) {
+    var oldsig = getSig(oldpmf);
     var pmf = [];
-    var minroll = 0;
-    var maxroll = 0;
-    var support = 1;
-    for (var d=0; d < dice.length; d++) // die type
-    {
-      sides = dice[d][1];
-      for (var r=0; r < dice[d][0]; r++) // times to roll
-      {
-        minroll += 1;
-        maxroll += sides;
-        support += sides - 1;
-        if (pmf.length === 0)   // base case
-          pmf = unif(sides);
-        else
-          pmf = funcmap[funcname](pmf, sides, support);
-      }
-    }
+    var support = Math.min(oldpmf.length, sides);
+    var p = 0;
+    for (var z=0; z < support; z++)
+      pmf.push((oldpmf[z] * (sides - z - 1) + oldsig[z]) / sides);
     return pmf;
   }
 
@@ -101,6 +101,42 @@ $(function() {
       cdf.push(cumsum);
     }
     return cdf;
+  }
+
+  // Return P(X >= x) for X~pmf
+  // TODO: factor out commonalities with getCdf
+  function getSig(pmf) {
+    var sig = []
+    var cumsum = 1;
+    for (var i=0; i < pmf.length; i++) {
+      sig.push(cumsum);
+      cumsum -= pmf[i];
+    }
+    return sig;
+  }
+
+  var funcmap = {
+    sum : convolute,
+    max : convomax,
+    min : convomin
+  }
+
+  // Return Probability Mass Function over function of dice roll
+  function getPmf(funcname, dice) {
+    var sides;
+    var pmf = [];
+    for (var d=0; d < dice.length; d++) // die type
+    {
+      sides = dice[d][1];
+      for (var r=0; r < dice[d][0]; r++) // times to roll
+      {
+        if (pmf.length === 0)   // base case
+          pmf = unif(sides);
+        else
+          pmf = funcmap[funcname](pmf, sides);
+      }
+    }
+    return pmf;
   }
 
   // Make it pretty
@@ -136,18 +172,37 @@ $(function() {
   });
 
   // Display logic
-  window.makePlot = function(funcname, dicestring, needCdf) {
+  window.makePlot = function(dist, funcname, dicestring) {
+    // Process input
     if (dicestring.length === 0)
       return;
     var dice = getDice(dicestring);
     var data = getPmf(funcname.toLowerCase(), dice);
-    var yText = 'Pr(X=x)';
-    var yMax = null;
-    if (needCdf) {
+    var yMax, cmpchar;
+    if (funcname.toLowerCase() !== 'sum')
+      dice.minroll = 1;
+    dist = dist.toLowerCase();
+    switch (dist) {
+    case 'cdf':
       data = getCdf(data);
-      yText = 'Pr(X\u2264x)'; // Pr(X <= x)
       yMax = 1;
+      cmpchar = '\u2264'; // <=
+      break;
+    case 'sig':
+      data = getSig(data);
+      yMax = 1;
+      cmpchar = '\u2265'; // >=
+      break;
+    default: // 'pdf'
+      yMax = null;
+      cmpchar = '=';
+      break;
     }
+    var yText = 'Pr(X' + cmpchar + 'x)';
+    function getTooltip() {
+      return 'Pr(X' + cmpchar + this.x + ') is ' + parseFloat(this.y.toFixed(5));
+    }
+    // Build display
     $('#diceplot').highcharts({
         title: {
           text: funcname + '(' + dice.stringform + ')'
@@ -160,8 +215,11 @@ $(function() {
         yAxis: {
           max: yMax,
           title: {
-            text: yText,
+            text: yText
           }
+        },
+        tooltip: {
+          formatter: getTooltip
         },
         series: [{
             name: yText,
@@ -172,5 +230,5 @@ $(function() {
   }
 
   // Default on page load
-  window.makePlot("Sum", "2d6", false);
+  window.makePlot("pdf", "Sum", "2d6");
 });
